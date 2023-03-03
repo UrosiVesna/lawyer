f2944d9e8c89df81d45944449377edbe39d4abaf
Uroš Zonjić <uros1987zonj1c@gmail.com>
2023-03-03 13:35:02 +0100

    Team section

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,28 +1,52 @@
 
 
-$(document).ready(function(){
-    
-    
-     function animation(){
-        
+$(document).ready(function () {
+
+
+    function animation() {
+
         let windowHeight = $(window).height();
         let scroll = $(window).scrollTop();
-        
-        $('.animation').each(function(){
-            
+
+        $('.animation').each(function () {
+
             let position = $(this).offset().top;
             let animationName = $(this).attr('data-animation');
-            
-            if(position < windowHeight + scroll - 100){
+
+            if (position < windowHeight + scroll - 100) {
                 $(this).addClass(animationName);
-            } 
-        }) ; 
+            }
+        });
     }
-    
-    $(window).scroll(function(){
+
+    $(window).scroll(function () {
         animation();
     });
-    
+
     animation();
+
+
+    //owl Carousel
+    
+if($('.team-members-slider').length > 0)
+    $(document).ready(function () {
+        $(".team-members-slider").owlCarousel({
+            
+            loop: true,
+            autoplay: true,
+            responsive:{
+                0:{
+                    items: 1,
+                    margin: 0
+                },
+                992: {
+                    items: 2,
+                    margin: 12
+                }
+            }
+        });
+    });
+
 });
+
 
